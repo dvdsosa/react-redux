24fe08352e520c991204970b50e85efac6dbcc0f
David <dvdsosa@gmail.com>
2018-07-19 14:37:47 +0100

    clase 23 - usando datos normalizados en las playlist

---
+++ src/entries/home.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { hydrate, render } from 'react-dom';
+import Home from '../pages/containers/home';
+// import Playlist from './src/playlist/components/playlist';
+// import data from '../api.json';
+// console.log('Hola mundo!' )
+import data from '../schemas/index'
+import { Provider } from 'react-redux';
+
+import { createStore } from 'redux';
+import reducer from '../reducers/data'
+
+// console.log(normalizedData)
+console.log(data)
+
+const initialState = {
+    data: {
+        // ...data
+        entities: data.entities,
+        categories: data.result.categories
+    },
+    search: []
+}
+
+const store = createStore(
+    reducer,
+    // (state) => state,
+    // initialState,
+    initialState,
+    // enhancer
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+// console.log(store.getState)
+
+const homeContainer = document.getElementById('home-container')
+
+
+// ReactDOM.render(que voy a renderizar, donde lo haré);
+// const holaMundo = <h1>hola Estudiante!</h1>;
+render( 
+    <Provider store={store}>
+        {/* <Home hola="ola k ase" /> */}
+        <Home />
+    </Provider> ,
+homeContainer);
+
---
+++ src/reducers/data.js
@@ -0,0 +1,22 @@
+function data(state, action) {
+    switch (action.type) {
+        case 'SEARCH_VIDEO': {
+            let results = []
+            if(action.payload.query) {
+                state.data.categories.map((item)=>{
+                    return item.playlist.filter( item => {
+                        item.author.toLowerCase().includes(action.payload.query.toLowerCase()) && results.push(item)
+                    })
+                })
+            }
+            return {
+                ...state,
+                search: results
+            }
+        }
+        default:
+            return state
+    }
+}
+
+export default data
